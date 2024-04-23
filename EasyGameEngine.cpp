3900e3d1755dcaa8edb3aa9eedfb221928b82973
Artyom Zabroda <artyomzabroda@gmail.com>
2024-04-23 23:24:11 +0300

    Добавьте файлы проекта.

---
+++ EasyGameEngine.cpp
@@ -0,0 +1,12 @@
+﻿// EasyGameEngine.cpp: определяет точку входа для приложения.
+//
+
+#include "EasyGameEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
